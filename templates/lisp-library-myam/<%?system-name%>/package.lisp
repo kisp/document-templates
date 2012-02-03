fe7961148f71dc02a526aa4e29aa550a42976e94
Kilian Sprotte <kilian.sprotte@gmail.com>
2012-02-03 02:58:17 +0100

    Moved templates to other repo.

--- templates/lisp-library-myam/<%?system-name%>/package.lisp
+++
@@ -1,6 +0,0 @@
-;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; Coding:utf-8 -*-
-;;; <%?system-name%> package definition
-
-(defpackage :<%?system-name%>
-  (:use :common-lisp)
-  (:export))
--- templates/ruby-gem-with-bin/<%(underscores ?gem-name)%>/lib/<%(underscores ?gem-name)%>.rb
+++
@@ -1,5 +0,0 @@
-module <%(camel-case ?gem-name)%>
-  def <%(camel-case ?gem-name)%>.main()
-    puts "His this is the main of <%(underscores ?gem-name)%>."
-  end
-end
