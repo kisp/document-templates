a1804ddcee6e4edffc82134af10aee310e3856fb
Kilian Sprotte <kilian.sprotte@gmail.com>
2019-10-26 21:39:30 +0200

    whitespace cleanup

--- .sbcl-disable-debugger.lisp
+++ .sbcl-disable-debugger.lisp
@@ -1,11 +1,11 @@
 (setq *debugger-hook*
-  (lambda (c h)
-    (declare (ignore h))
-    (format *error-output* "ERROR of type ~S:~%~A~%" (type-of c) c)
-    (sb-ext:exit :code 1)))
+      (lambda (c h)
+        (declare (ignore h))
+        (format *error-output* "ERROR of type ~S:~%~A~%" (type-of c) c)
+        (sb-ext:exit :code 1)))
 (setq sb-ext:*invoke-debugger-hook* *debugger-hook*)
 
-(defmacro without-warnings (&body body) 
+(defmacro without-warnings (&body body)
   `(handler-bind ((warning #'muffle-warning)
-		 #+sbcl (sb-ext:compiler-note #'muffle-warning))
-    ,@body))
+                  #+sbcl (sb-ext:compiler-note #'muffle-warning))
+     ,@body))
